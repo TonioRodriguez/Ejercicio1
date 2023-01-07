e785ec8da5c5ab3c1e1e9db442e8e46db19185e3
TonioRodriguez <luisrodriguez.dwfc5@ucamp.utel.edu.mx>
2023-01-07 13:36:35 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let dato = window.prompt("Introduce un número", "0");
+
+let datoS = window.prompt("introduce un número", "0");
+
+let resta, multiplicacion;
+resta = dato - datoS;
+multiplicacion = dato * datoS;
+
+alert(`El resultado de la resta es ${resta}`);
+alert(`El resultado de la multiplicación es ${multiplicacion}`);
